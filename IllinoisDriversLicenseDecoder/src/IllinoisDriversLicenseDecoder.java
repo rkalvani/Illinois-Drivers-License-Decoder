2abe9007a6ce6d49b4bbe7f27862ff0d6361d26b
rbkal <rbkal@DESKTOP-5ELGI3C>
2017-12-02 14:03:56 -0600

    Finished typing up lastThree digits function to solve for your gender and name as well as the skeleton of what sub functions we need and how the main function will operate.

--- IllinoisDriversLicenseDecoder/src/IllinoisDriversLicenseDecoder.java
+++ IllinoisDriversLicenseDecoder/src/IllinoisDriversLicenseDecoder.java
@@ -2,8 +2,40 @@
 public class IllinoisDriversLicenseDecoder {
 
 	public static void main(String[] args) {
-		System.out.println("wow!");
-		//a test change
+		String fullLicenseNumber = "            "; //12 spaces as a placeholder for the number
+		//decodes your gender and birthday
+		lastThreeDigits(fullLicenseNumber.substring(9));
+		//year of birth
+		System.out.println(fullLicenseNumber.substring(7, 9));
+		//solves for your initials
+		initials(fullLicenseNumber.substring(0, 7));
+		//last initial
+		System.out.println(fullLicenseNumber.charAt(0));
 	}
 
+	public static void lastThreeDigits(String string) {
+		int num = Integer.parseInt(string);
+		if (num >= 601) {
+			num -= 600;
+			System.out.println("You are a female.");
+		} else {
+			System.out.println("You are a male.");
+		}
+		System.out.print("Your birthday is ");
+		int day = num % 31;
+		num -= day;
+		if (num < 10) {
+			System.out.print("0");
+		}
+		System.out.print(num + "/");
+		if (day < 10) {
+			System.out.print("0");
+		}
+		System.out.print(day + "/");
+		
+	}
+	
+	public static void initials(String string) {
+		//calculating your initials
+	}
 }
